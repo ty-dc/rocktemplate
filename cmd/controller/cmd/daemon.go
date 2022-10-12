1072284665641f493a0ebe75534c0eb29ee6ded5
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2022-10-12 20:46:12 +0800

    asdf
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

--- cmd/controller/cmd/daemon.go
+++ cmd/controller/cmd/daemon.go
@@ -11,7 +11,7 @@ import (
 )
 
 func SetupUtility() {
-	c := make(chan os.Signal)
+	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGUSR1, syscall.SIGUSR2)
 	go func() {
 		for s := range c {
